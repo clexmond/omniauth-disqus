dfad940f0f8b622b47f5862071bd7cd54e1988d8
Matija Čupić <matteeyah@gmail.com>
2020-05-25 03:42:04 +0200

    Fix rubocop offenses

--- lib/omniauth/strategies/disqus.rb
+++ lib/omniauth/strategies/disqus.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'omniauth-oauth2'
 
 module OmniAuth
@@ -7,21 +9,21 @@ module OmniAuth
       option :scope, 'read'
 
       option :client_options, {
-        :site          => 'https://disqus.com',
-        :authorize_url => '/api/oauth/2.0/authorize/',
-        :token_url     => '/api/oauth/2.0/access_token/'
-        }
+        site: 'https://disqus.com',
+        authorize_url: '/api/oauth/2.0/authorize/',
+        token_url: '/api/oauth/2.0/access_token/'
+      }
 
       uid { access_token.params['user_id'] }
 
       info do
         {
-          :name        => raw_info['name'],
-          :nickname    => raw_info['username'],
-          :location    => raw_info['location'],
-          :description => raw_info['about'],
-          :image       => raw_info['avatar']['small']['permalink'],
-          :urls        => {
+          name: raw_info['name'],
+          nickname: raw_info['username'],
+          location: raw_info['location'],
+          description: raw_info['about'],
+          image: raw_info['avatar']['small']['permalink'],
+          urls: {
             'profileUrl' => raw_info['profileUrl']
           }
         }
@@ -29,22 +31,22 @@ module OmniAuth
 
       extra do
         {
-          :raw_info => raw_info
+          raw_info: raw_info
         }
       end
 
       def callback_url
         full_host + script_name + callback_path
       end
-      
+
       def raw_info
         url    = '/api/3.0/users/details.json'
         params = {
-          'api_key'      => access_token.client.id,
+          'api_key' => access_token.client.id,
           'access_token' => access_token.token
         }
 
-        @raw_info ||= access_token.get(url, :params => params).parsed['response']
+        @raw_info ||= access_token.get(url, params: params).parsed['response']
       end
     end
   end
